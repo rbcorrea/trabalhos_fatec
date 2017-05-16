b5300d00c71652a7cd978a342cb5d7d8e8e7850c
Rodrigo Baptista Correa <r.odrigoxcorrea@gmail.com>
2017-05-16 20:21:51 -0300

    Alterei exibição
    bla

--- trabalho_fatec/main.c
+++ trabalho_fatec/main.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
             break;
 
         default:
-            printf("Coloque um argumento! \n")
+            printf("Coloque um argumento para ontinuar a excução! \n")
             return 0;
             break;
     }
