e09ad6137fb887fcbde6c3f41aa3031a62c617af
Rodrigo Baptista Correa <r.odrigoxcorrea@gmail.com>
2017-05-16 20:18:01 -0300

    Teste com args do main

---
+++ trabalho_fatec/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    if(argc<2){ // caso onde não há argumentos;
+
+        printf("Coloque algum argumento! \n");
+        return 0;
+
+    }else{
+        switch argc[2]{
+        case ('i' || 'I'):
+            printf("Você selecionou a opção Invert! \n");
+
+            break;
+
+        case ('c' || 'C') :
+            printf("vocÊ selecionou a opção Cript! \n");
+
+            break;
+        case ('u' || 'U'):
+            printf("Voce selecionou a opção Unix-To-Dos! \n");
+
+            break;
+
+        case ('d' || 'D'):
+            printf("Voce selecionou a opção Dos-To-Unix");
+            break;
+
+        default:
+            printf("Coloque um argumento! \n")
+            return 0;
+            break;
+    }
+
+    return 0;
+}
