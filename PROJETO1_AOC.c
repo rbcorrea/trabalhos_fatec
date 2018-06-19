95f03f27c37a2d0175b424c757e7b0f785494765
Rodrigo Baptista Correa <rodrigoxcorrea@gmail.com>
2018-06-19 12:34:52 -0300

    Update PROJETO1_AOC.c

--- PROJETO1_AOC.c
+++ PROJETO1_AOC.c
@@ -24,15 +24,62 @@ char randomChar(int operacao){
 	
 }
 
+char * readline(FILE *fp, char *buffer)
+{
+    int ch;
+    int i = 0;
+    size_t buff_len = 0;
+
+    buffer = malloc(buff_len + 1);
+    if (!buffer) return NULL;  // Out of memory
+
+    while ((ch = fgetc(fp)) != '\n' && ch != EOF)
+    {
+        buff_len++;
+        void *tmp = realloc(buffer, buff_len + 1);
+        if (tmp == NULL)
+        {
+            free(buffer);
+            return NULL; // Out of memory
+        }
+        buffer = tmp;
+
+        buffer[i] = (char) ch;
+        i++;
+    }
+    buffer[i] = '\0';
+
+    // Detect end
+    if (ch == EOF && (i == 0 || ferror(fp)))
+    {
+        free(buffer);
+        return NULL;
+    }
+    return buffer;
+}
+
+void lineByline(FILE * file){
+char *s;
+while ((s = readline(file, 0)) != NULL)
+{
+    puts(s);
+    free(s);
+    printf("\n");
+}
+}
+
 int main(int argc, char *argv[]) {
-	char ch, operacao;
-	FILE *fp;
+	char ch, operacao, *arquivo;
+	FILE* fp = fopen("RA.TXT", "r");;
 	char linha[7];
 	
-	printf("---------------------------------\n");
-	printf("Imprimindo um caracter aleatorio\n");
-	printf("%c\n", randomChar(3));
-	printf("---------------------------------\n");
+	lineByline(fp);
+	
+	
+	//printf("---------------------------------\n");
+	//printf("Imprimindo um caracter aleatorio\n");
+	//printf("%c\n", randomChar(3));
+	//printf("---------------------------------\n");
 	
 	//printf("Qual operacao voce deseja efetuar? 1 - Senha Numerica | 2 - Senha Alfanumerica");
 	//do(scanf(&operacao)){
